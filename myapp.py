8379754ab9eb0e7008baaa9c48f3fa23c0dfabae
Pavitra Singh <PavitraSingh@pc-73-54.customer.stratford.derwent.lan>
2019-03-26 01:46:11 +0000

    fourth commit

--- myapp.py
+++ myapp.py
@@ -4,18 +4,27 @@ from plotly.utils import PlotlyJSONEncoder
 import json
 import requests
 import requests_cache
-"""
 from cassandra.cluster import Cluster
-
-cluster = Cluster(['cassandra'])
-session = cluster.connect()
-app = Flask(__name__)
-"""
-requests_cache.install_cache('exchange_api_cache', backend='sqlite', expire_after=36000)
+import uuid
+from flask import Flask
+from flask.ext.cqlalchemy import CQLAlchemy
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
+
+app.config['CASSANDRA_HOSTS'] = ['127.0.0.1']
+app.config['CASSANDRA_KEYSPACE'] = "cqlengine"
+db = CQLAlchemy(app)
+
+class Forex(db.Model):
+    Country = db.columns.Text(required=False)
+    Country_code = db.columns.Text(required=False)
+    Currency = db.columns.Text(required=False)
+    Currency_code = db.columns.Text(required=False)
+
+
+requests_cache.install_cache('exchange_api_cache', backend='sqlite', expire_after=36000)
 
 market_url= 'https://forex.1forge.com/1.0.3/market_status?api_key={key}'
 forex_url= 'https://forex.1forge.com/1.0.3/quotes?pairs={pairs}&api_key={key}'
@@ -61,6 +70,48 @@ def convert():
     else:
         return (resp.reason)
 
+
+@app.route('/entryCode', methods=['POST'])
+def entry_code():
+    ''' create or add new code '''
+    data = request.get_json()
+    if data['ok']:
+        data = data['data']
+        person = Forex(Country= data['Country'], Country_code= data['Country_code'],
+                    Currency= data['Currency'], Currency_code= data['Currency_code'])
+        person.save()
+        return jsonify({'ok': True, 'data': data, 'message': 'Country code added successfully!'}), 200
+
+    else:
+        return jsonify({'ok': False, 'message': 'Bad request parameters: {}'.format(data['message'])}), 400
+
+
+@app.route('/code', methods=['GET', 'DELETE'])
+def code():
+    ''' route read user '''
+    if request.method == 'GET':
+        query = request.args
+        data = Forex.get('Country'= query['Country'])
+        if bool(data):
+            user = {}
+            user['Country'] = data['Country']
+            user['Code'] = data['Code']
+
+            return jsonify({'ok': True, 'data': user}), 200
+        else:
+            return jsonify({'ok': False, 'message': 'No user exist with this mail'}), 400
+
+    data = request.get_json()
+    if request.method == 'DELETE':
+             query = Forex.get('Country'= data['Country'])
+            if bool(query):
+                db_response = Forex.delete('Country' = data['Country']
+                response = {'ok': True, 'message': 'record deleted'}
+            else:
+                response = {'ok': True, 'message': 'no record found'}
+            return jsonify(response), 200
+        else:
+            return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
 
 
 if __name__=="__main__":
